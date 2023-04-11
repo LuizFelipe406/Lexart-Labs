c59c4c4a82fd6b37d6fc3c1855c66fc48aafb41f
luiz felipe <luizfpereira406@gmail.com>
2023-04-11 12:09:29 -0300

    fix: Fix MercadoLivre api endpoint

--- frontend/src/api/MercadoLivre.ts
+++ frontend/src/api/MercadoLivre.ts
@@ -1,9 +1,9 @@
 import { Filters } from "../context";
 
 const CATEGORIES = {
-  "TV": "MLB1002",
-  "Mobile": "MLB1055",
-  "Refrigerator": "MLB181294"
+  TV: "MLB1002",
+  Mobile: "MLB1055",
+  Refrigerator: "MLB181294"
 }
 
 interface ApiProduct {
@@ -16,7 +16,7 @@ interface ApiProduct {
 
 export const searchInMercadoLivre = async ({ category, name }: Filters): Promise<ApiProduct[]> => {
   const categoryId = CATEGORIES[category]
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${name}`;
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${name}`;
   const request = await fetch(endpoint);
   const result = await request.json()
   return result.results
