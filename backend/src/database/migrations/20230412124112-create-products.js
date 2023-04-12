913b00ffb7f8b0a7277e56b815cab514405bc57d
luiz felipe <luizfpereira406@gmail.com>
2023-04-12 10:00:29 -0300

    feat: Setup database with Sequelize

---
+++ backend/src/database/migrations/20230412124112-create-products.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+      },
+      filterId: {
+        type: Sequelize.INTEGER,
+        field: "filter_id",
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Products',
+          key: 'id'
+        }
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      website: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ backend/src/database/models/Filter.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from './index';
+
+class Filter extends Model {
+  declare id: number;
+  declare name: string;
+  declare source: string;
+  declare category: string;
+}
+
+Filter.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  source: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+  modelName: 'Filter',
+  tableName: 'Filters',
+  timestamps: false,
+  sequelize: sequelize,
+});
+
+export default Filter;
