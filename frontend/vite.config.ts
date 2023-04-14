33e15c6be58a98f63068d59ee0238be42cfb5211
luiz felipe <luizfpereira406@gmail.com>
2023-04-14 11:45:44 -0300

    chore: testing vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from "dotenv"
-
-dotenv.config()
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
   plugins: [react()],
   server: {
     host: true,
@@ -13,6 +13,7 @@ export default defineConfig({
   },
   envPrefix: 'VITE_',
   define: {
-    dotenv: process.env
+    __APP_ENV__: env.APP_ENV,
   }
+}
 })
