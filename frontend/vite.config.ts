e5a7fdfcdf5bb5f3fc3139de104add994cad8e2d
luiz felipe <luizfpereira406@gmail.com>
2023-04-14 11:42:05 -0300

    chore: testing things in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv"
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +11,8 @@ export default defineConfig({
     host: true,
     port: 3000,
   },
-  envPrefix: 'VITE_'
+  envPrefix: 'VITE_',
+  define: {
+    dotenv: process.env
+  }
 })
